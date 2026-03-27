5a8f8786f3d394f85c0157976469cc30b2c5ebfa
SantianDev <cubexteams@gmail.com>
2026-03-27 10:07:01 +0300

    refactor: composter & fix: item count in composter

--- src/main/java/cn/nukkit/block/BlockComposter.java
+++ src/main/java/cn/nukkit/block/BlockComposter.java
@@ -1,12 +1,17 @@
 package cn.nukkit.block;
 
 import cn.nukkit.Player;
+import cn.nukkit.block.material.BlockType;
+import cn.nukkit.block.material.tags.BlockInternalTags;
+import cn.nukkit.block.material.tags.BlockTag;
 import cn.nukkit.event.block.ComposterEmptyEvent;
 import cn.nukkit.event.block.ComposterFillEvent;
 import cn.nukkit.item.*;
+import cn.nukkit.item.material.ItemType;
+import cn.nukkit.item.material.tags.ItemTag;
+import cn.nukkit.level.ParticleEffect;
 import cn.nukkit.level.Sound;
-import cn.nukkit.item.data.DyeColor;
-import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nonnull;
@@ -14,22 +19,18 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class BlockComposter extends BlockSolidMeta implements ItemID {
 
-    private static final Int2IntOpenHashMap ITEMS = new Int2IntOpenHashMap();
+    private static final Object2IntOpenHashMap<String> ITEMS = new Object2IntOpenHashMap<>();
 
     static {
-        registerItems(30, BEETROOT_SEEDS, DRIED_KELP, GLOW_BERRIES, KELP, MELON_SEEDS, PUMPKIN_SEEDS, SWEET_BERRIES, WHEAT_SEEDS);
-        registerItems(50, MELON_SLICE, NETHER_SPROUTS, SUGAR_CANE);
-        registerItems(65, APPLE, BEETROOT, CARROT, COCOA, POTATO, NETHER_WART, WHEAT);
-        registerItems(85, BAKED_POTATOES, BREAD, COOKIE);
-        registerItems(100, CAKE, PUMPKIN_PIE);
-        registerBlocks(30, GRASS, LEAVES, LEAVES2, SAPLINGS, SEAGRASS, SMALL_DRIPLEAF, MANGROVE_PROPAGULE, MANGROVE_ROOTS);
-        registerBlocks(50, CACTUS, DRIED_KELP_BLOCK, AZALEA_LEAVES_FLOWERED, GLOW_LICHEN, TWISTING_VINES, VINES, WEEPING_VINES);
-        registerBlocks(65, AZALEA, BIG_DRIPLEAF, DOUBLE_PLANT, DANDELION, RED_FLOWER, WITHER_ROSE, CRIMSON_FUNGUS, WARPED_FUNGUS, LILY_PAD, MELON_BLOCK, MOSS_BLOCK, BROWN_MUSHROOM, RED_MUSHROOM, PUMPKIN, CARVED_PUMPKIN, CRIMSON_ROOTS, WARPED_ROOTS, SEA_PICKLE, SHROOMLIGHT);
-        registerBlocks(85, FLOWERING_AZALEA, HAY_BALE, BROWN_MUSHROOM_BLOCK, RED_MUSHROOM_BLOCK, BLOCK_NETHER_WART_BLOCK, PITCHER_PLANT, TORCHFLOWER, WARPED_WART_BLOCK);
-        registerBlock(50, TALL_GRASS, 0);
-        registerBlock(50, TALL_GRASS, 1);
-        registerBlock(65, TALL_GRASS, 2);
-        registerBlock(65, TALL_GRASS, 3);
+        registerByBlockTag(BlockInternalTags.SAPLING, 30);
+        registerByBlockTag(BlockInternalTags.LEAVES, 30);
+        registerByBlockTag(BlockInternalTags.FLOWERS, 65);
+        register(30, ItemNamespaceId.BEETROOT_SEEDS, ItemNamespaceId.BUSH, ItemNamespaceId.CACTUS_FLOWER, ItemNamespaceId.DRIED_KELP, ItemNamespaceId.FIREFLY_BUSH, ItemNamespaceId.GLOW_BERRIES, ItemNamespaceId.GRASS_BLOCK, ItemNamespaceId.HANGING_ROOTS, ItemNamespaceId.KELP, ItemNamespaceId.LEAF_LITTER, ItemNamespaceId.MANGROVE_PROPAGULE, ItemNamespaceId.MANGROVE_ROOTS, ItemNamespaceId.MELON_SEEDS, ItemNamespaceId.MOSS_CARPET, ItemNamespaceId.PALE_HANGING_MOSS, ItemNamespaceId.PALE_MOSS_CARPET, ItemNamespaceId.PINK_PETALS, ItemNamespaceId.PITCHER_POD, ItemNamespaceId.PUMPKIN_SEEDS, ItemNamespaceId.SEAGRASS, ItemNamespaceId.SHORT_GRASS, ItemNamespaceId.SHORT_DRY_GRASS, ItemNamespaceId.SMALL_DRIPLEAF_BLOCK, ItemNamespaceId.SWEET_BERRIES, ItemNamespaceId.TALL_DRY_GRASS, ItemNamespaceId.TORCHFLOWER_SEEDS, ItemNamespaceId.WHEAT_SEEDS, ItemNamespaceId.WILDFLOWERS);
+        register(50, ItemNamespaceId.CACTUS, ItemNamespaceId.DRIED_KELP_BLOCK, ItemNamespaceId.AZALEA_LEAVES_FLOWERED, ItemNamespaceId.GLOW_LICHEN, ItemNamespaceId.MELON_SLICE, ItemNamespaceId.NETHER_SPROUTS, ItemNamespaceId.SUGAR_CANE, ItemNamespaceId.TALL_GRASS, ItemNamespaceId.TWISTING_VINES, ItemNamespaceId.VINE, ItemNamespaceId.WEEPING_VINES);
+        register(65, ItemNamespaceId.APPLE, ItemNamespaceId.AZALEA, ItemNamespaceId.BEETROOT, ItemNamespaceId.BIG_DRIPLEAF, ItemNamespaceId.CARROT, ItemNamespaceId.COCOA_BEANS, ItemNamespaceId.FERN, ItemNamespaceId.LARGE_FERN, ItemNamespaceId.WATERLILY, ItemNamespaceId.MELON_BLOCK, ItemNamespaceId.MOSS_BLOCK, ItemNamespaceId.RED_MUSHROOM, ItemNamespaceId.BROWN_MUSHROOM, ItemNamespaceId.MUSHROOM_STEM, ItemNamespaceId.CRIMSON_FUNGUS, ItemNamespaceId.WARPED_FUNGUS, ItemNamespaceId.PALE_MOSS_BLOCK, ItemNamespaceId.POTATO, ItemNamespaceId.PUMPKIN, ItemNamespaceId.CARVED_PUMPKIN, ItemNamespaceId.CRIMSON_ROOTS, ItemNamespaceId.WARPED_ROOTS, ItemNamespaceId.SEA_PICKLE, ItemNamespaceId.SHROOMLIGHT, ItemNamespaceId.SPORE_BLOSSOM, ItemNamespaceId.WHEAT, ItemNamespaceId.WITHER_ROSE);
+        register(85, ItemNamespaceId.BAKED_POTATO, ItemNamespaceId.BREAD, ItemNamespaceId.COOKIE, ItemNamespaceId.FLOWERING_AZALEA, ItemNamespaceId.HAY_BLOCK, ItemNamespaceId.BROWN_MUSHROOM_BLOCK, ItemNamespaceId.RED_MUSHROOM_BLOCK, ItemNamespaceId.NETHER_WART_BLOCK, ItemNamespaceId.PITCHER_PLANT, ItemNamespaceId.TORCHFLOWER, ItemNamespaceId.WARPED_WART_BLOCK);
+        register(100, ItemNamespaceId.CAKE, ItemNamespaceId.PUMPKIN_PIE);
+        register(ItemNamespaceId.GOLDEN_DANDELION, 0); //Override chance registered by flowers tag
     }
 
     public BlockComposter() {
@@ -131,9 +132,11 @@ public class BlockComposter extends BlockSolidMeta implements ItemID {
 
         if (player != null &&!player.isCreative()) {
             item.setCount(item.getCount() - 1);
+            player.getInventory().setItemInHand(item);
         }
 
         if (event.isSuccess()) {
+            level.addParticleEffect(this.add(0.5, 0.5, 0.5), ParticleEffect.CROP_GROWTH, -1, level.getDimension(), (Player[]) null);
             if (this.incrementLevel()) {
                 level.addSound(this.add(0.5, 0.5, 0.5), Sound.BLOCK_COMPOSTER_READY);
             } else {
@@ -171,46 +174,29 @@ public class BlockComposter extends BlockSolidMeta implements ItemID {
         return null;
     }
 
-    public static void registerItem(int chance, int itemId) {
-        registerItem(chance, itemId, 0);
-    }
-
-    public static void registerItem(int chance, int itemId, int meta) {
-        ITEMS.put(itemId << 6 | meta & 0x3F, chance);
-    }
-
-    public static void registerItems(int chance, int... itemIds) {
-        for (int itemId : itemIds) {
-            registerItem(chance, itemId, 0);
+    public static void register(int value, String... keys) {
+        for(String key : keys) {
+            register(key, value);
         }
     }
 
-    public static void registerBlocks(int chance, int... blockIds) {
-        for (int blockId : blockIds) {
-            registerBlock(chance, blockId, 0);
+    public static void register(String key, int value) {
+        ITEMS.put(key, value);
+    }
+
+    public static void registerByItemTag(ItemTag tag, int value) {
+        for (ItemType type : tag.getItemTypes()) {
+            register(type.getIdentifier(), value);
         }
     }
 
-    public static void registerBlock(int chance, int blockId) {
-        registerBlock(chance, blockId, 0);
-    }
-
-    public static void registerBlock(int chance, int blockId, int meta) {
-        if (blockId > 255) {
-            blockId = 255 - blockId;
+    public static void registerByBlockTag(BlockTag tag, int value) {
+        for (BlockType type : tag.getBlockTypes()) {
+            register(type.getIdentifier(), value);
         }
-        registerItem(chance, blockId, meta);
-    }
-
-    public static void register(int chance, Item item) {
-        registerItem(chance, item.getId(), item.getDamage());
     }
 
     public static int getChance(Item item) {
-        int chance = ITEMS.get(item.getId() << 6 | item.getDamage());
-        if (chance == 0) {
-            chance = ITEMS.get(item.getId() << 6);
-        }
-        return chance;
+        return ITEMS.getOrDefault(item.getNamespaceId(), 0);
     }
 }
